07f1c3b62a430c107ac79e7019b98b2a5675cfd1
kimyoungwoon <ingngnin@gmail.com>
2022-08-23 14:39:02 +0900

    struts2 to springboot change

---
+++ wordleServer/src/main/java/com/game/wordleServer/WordleServerApplication.java
@@ -0,0 +1,13 @@
+package com.game.wordleServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordleServerApplication.class, args);
+	}
+
+}
---
+++ wordleServer/src/test/java/com/game/wordleServer/WordleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.wordleServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
